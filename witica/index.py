0d87fd0e7a1876aa950e677cd71c5da9ed346797
Nils Breyer <nbnet@arcor.de>
2015-02-18 20:23:51 +0100

    continue Index class, add ItemIndex class

--- witica/index.py
+++ witica/index.py
@@ -37,13 +37,31 @@ class Index(AsyncWorker):
 		site.source.stoppedEvent += lambda sender, args: self.close_queue()
 
 	def get_state_filename(self):
-		return cache_folder + os.sep + self.source_id + "#" + self.index_id + ".index"
+		return cache_folder + os.sep + self.site.source.source_id + "#" + self.index_id + ".index"
+
+	def get_cache_dir(self):
+		return cache_folder + os.sep + self.site.source.source_id + "#" + self.index_id
+	
+	def init(self): #no state file found, create clean cache folder
+		self.state["source_cursor"] = ""
+		if os.path.isdir(self.get_cache_dir()):
+			shutil.rmtree(self.get_cache_dir())
+		os.makedirs(self.get_cache_dir())
+
+	def destroy(self):
+		self.save_state()
+		if os.path.isdir(self.get_cache_dir()):
+			shutil.rmtree(self.get_cache_dir())
+		os.remove(self.get_state_filename())
 
 	@staticmethod
-	def construct_from_json (source_id, index_id, config):
+	def construct_from_json (site, index_id, config):
 		classes = Index.get_classes()
-		instance = classes[config["type"]](source_id, target_id, config)
-		return instance
+
+		if "type" in config:
+			return classes[config["type"]](site, index_id, config)
+		else: #default: ItemIndex
+			return ItemIndex(site, index_id, config)
 
 	@staticmethod
 	def get_classes():
@@ -80,3 +98,28 @@ class Index(AsyncWorker):
 	@abstractmethod
 	def get_page_count(self):
 		pass
+
+
+class ItemIndex(Index):
+	"""docstring for ItemIndex"""
+	def __init__(self, site, index_id, config):
+		super(ItemIndex, self).__init__(site, index_id, config)
+
+	def is_relevant(self, event):
+		return True
+
+	def update_item(self, item):
+		pass
+
+	def remove_item(self, item):
+		pass
+
+	def get_index_filename(self,page):
+		pass
+
+	def get_metadata(self):
+		pass
+
+	def get_page_count(self):
+		pass
+		
