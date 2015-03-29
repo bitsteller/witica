d4cbaeb00f394d87e967546062c1477eac58ea7a
Nils Breyer <nbnet@arcor.de>
2015-03-29 18:41:44 +0200

    fix nested json in md files: empty line as separator is now always required

--- witica/metadata/extractor.py
+++ witica/metadata/extractor.py
@@ -18,7 +18,7 @@ RE_ITEM_REFERENCE = r'^!(?:.\/)?' + RE_FIRST_ITEMID + '$'
 RE_META_KEYSPEC = r'^([<>]?)([\s\S]*)$'
 
 #regular expressions to be used for md files parsing
-RE_MD_SPLIT_JSON_MD = "^\s*({[\s\S]*?})?[\s]*([^}\s][\s\S]*)$" #splits md file into the json metadata and markdown sections as caputre groups
+RE_MD_SPLIT_JSON_MD = "^\s*({[\s\S]*?}\n)?[\s]*([^}\s][\s\S]*)$" #splits md file into the json metadata and markdown sections as caputre groups
 RE_MD_SPLIT_TITLE_BODY = "^(?:#(?!#)[\t ]*([\S][^\n\r]*)(?:\n|\r\n?|$))?([\s\S]*)$" #splits markdown section into title and body sections as capture groups
 
 RE_MD_NOBRACKET = r'[^\]\[]*'
